40ca7aa6a01ef9e8a9d8e340f13759dc85987f95
Artem <temka6397@mail.ru>
2022-08-10 13:01:14 +0500

    program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] result = CreateArray();
+ArraySort(result);
+
+void ArraySort(string[] arr)
+{
+    int size = 3; 
+    var value = string.Empty;
+    string[] newarr = new string[arr.Length]; 
+    for (int i = 0; i < arr.Length; i++)
+    {
+        value = arr[i]; 
+        if (value.Length <= size) 
+            newarr[i] = arr[i];
+    }
+    newarr = newarr.Where(x => x != null).ToArray(); 
+    Console.WriteLine($"Отобранный массив: [{string.Join(", ", newarr)}]");
+}
+
+string[] CreateArray()
+{
+    Console.WriteLine("Введите элементы массива через запятую: ");
+    string[] array = Console.ReadLine()!.Split(',');
+    Console.WriteLine($"Изначальный массив: [{string.Join(", ", array)}]");
+    return array;
+}
